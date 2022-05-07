9e8424e6117aa15ef99d0833254ecbbc28393fe0
55stella <stellarichards7@gmail.com>
2022-05-07 13:34:47 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import Data from './data'
 
 function App() {
+  const[count, setCount]= useState('')
+  const [data, setData] =useState([])
+  
+  
+const handleOnclick =(e)=>{
+e.preventDefault()
+    if(count > Data.length-1){
+      setData(Data)
+      setCount('')
+  
+    }if(count <= Data.length-1){
+      setData(Data.slice(0,count))
+      setCount('')
+
+    }if(count <= 0){
+      setData(Data.slice(-1))
+           
+    }
+    
+
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className='display'>
+        <h1>Tired of boring lorem ipsum ?</h1>
+        
+        <form className='lorem-form'>
+          <div>
+          <label htmlFor="amount">Paragraph:</label>
+          <input type="number"
+          id ='paragraph' 
+          name ='Paragraph'
+           value ={count}
+            onChange={(e)=>setCount(e.target.value)}
+            />
+         </div>
+         <button type ='button' onClick={handleOnclick}> GENERATE</button>
+        </form>
+      
+        {data.map((item, index)=>{
+          return <div key ={index}  className='lorem-text'>
+          <p>{item}</p>
+        </div>
+        })}
+        
+      </section>
+      
+    </main>
   );
 }
 
